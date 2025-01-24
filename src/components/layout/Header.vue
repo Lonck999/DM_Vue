4695b777edc716736dc081a04b62450ddde4679d
地瓜球工程師 <joe22053814@gmail.com>
2025-01-24 16:45:46 +0800

    Updata Header.vue

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,9 +1,40 @@
 <script setup>
 import logo from '@/assets/img/dm-logo.png'
+import fb from '@/assets/img/facebook-icon.png'
+import line from '@/assets/img/line-icon.png'
+import { RouterLink } from 'vue-router'
+import { reactive } from 'vue'
+
+const navList = reactive([
+  { name: '叫號進度', link: '' },
+  { name: '門診時間', link: '' },
+  { name: '醫療團隊', link: '' },
+  { name: '診所環境', link: '' },
+  { name: '聯絡我們', link: '' },
+  { name: '掛號查詢', link: '' },
+  { name: 'facebook', icon: fb, link: 'https://www.facebook.com/dermai.clinic/?locale=zh_TW' },
+  { name: 'line', icon: line, link: 'https://lin.ee/AY002Fz' },
+])
 </script>
 <template>
   <header class="header">
-    <img :src="logo" alt="logo" />
+    <RouterLink to="/">
+      <img :src="logo" alt="logo" />
+    </RouterLink>
+    <nav>
+      <ul>
+        <li v-for="item in navList" :key="item.name">
+          <template v-if="item.icon">
+            <a :href="item.link">
+              <img :src="item.icon" :alt="item.name" />
+            </a>
+          </template>
+          <template v-else>
+            <a :href="item.link">{{ item.name }}</a>
+          </template>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
