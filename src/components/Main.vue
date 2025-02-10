922ac04fece863b66fe5975b563a0b7c4de2a674
地瓜球工程師 <joe22053814@gmail.com>
2025-02-10 14:30:13 +0800

    Update Main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,39 @@
-<script setup></script>
+<script setup>
+import { reactive } from 'vue'
+
+const title = reactive({
+  titleH1: '立即加入!',
+  titleH2: '德美診所官方LINE帳號',
+  titleP1: '查詢',
+  titleSpan1: '門診時間',
+  titleSpan2: '看診進度',
+  titleSpan3: '預約掛號',
+  titleSpan4: '居家照護視訊診療',
+})
+</script>
 
 <template>
-  <div></div>
+  <main>
+    <div>
+      <div>
+        <h1>{{ title.titleH1 }}</h1>
+        <h2>{{ title.titleH2 }}</h2>
+      </div>
+      <div>
+        <p>
+          {{ title.titleP1 }}
+          <span>{{ title.titleSpan1 }}</span>
+          、
+          <span>{{ title.titleSpan2 }}</span>
+          、
+          <span>{{ title.titleSpan3 }}</span>
+          、
+          <span>{{ title.titleSpan4 }}</span>
+          。
+        </p>
+      </div>
+    </div>
+    <div></div>
+  </main>
 </template>
 <style></style>
