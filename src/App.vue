b0b70195725feeb7b97ea407c2f6dd2067b9bf7e
地瓜球工程師 <joe22053814@gmail.com>
2025-02-10 14:01:14 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
-import Header from './components/layout/Header.vue'
 import Footer from './components/layout/Footer.vue'
 </script>
 
 <template>
   <div class="container">
-    <Header />
     <router-view />
     <Footer />
   </div>
