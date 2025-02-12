80696f12b01369521225b6f89324f6f3070921b9
地瓜球工程師 <joe22053814@gmail.com>
2025-02-12 15:52:37 +0800

    Create Number.vue

---
+++ src/components/Number.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <div class="number">
+    <h1></h1>
+    <div></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.number {
+  width: min(100%, 192rem);
+}
+</style>
