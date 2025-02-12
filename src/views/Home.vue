c65b36a921e8f9e817473dc126420d2ab424471c
地瓜球工程師 <joe22053814@gmail.com>
2025-02-12 16:43:49 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Header from '@/components/layout/Header.vue'
 import Main from '@/components/Main.vue'
+import Number from '@/components/Number.vue'
 </script>
 
 <template>
   <div class="home">
     <Header />
     <Main />
+    <Number />
   </div>
 </template>
 
