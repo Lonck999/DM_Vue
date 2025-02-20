066253eb68383bdf2f2c28d4fcbdc8fe7ce0bb6c
地瓜球工程師 <joe22053814@gmail.com>
2025-02-20 17:46:16 +0800

    Update ScheduleRehabilitationDepartment.vue

--- src/components/ScheduleRehabilitationDepartment.vue
+++ src/components/ScheduleRehabilitationDepartment.vue
@@ -1,58 +1,61 @@
 <script setup>
 import { reactive, toRefs } from 'vue'
 const schedule = reactive({
-  week: '門診時間/星期',
-  monCH: '星期一',
-  monEN: 'MON',
-  tueCH: '星期二',
-  tueEN: 'TUE',
-  wedCH: '星期三',
-  wedEN: 'WED',
-  thuCH: '星期四',
-  thuEN: 'THU',
-  friCH: '星期五',
-  friEN: 'FRI',
-  satCH: '星期六',
-  satEN: 'SAT',
-  sunCH: '星期日',
-  sunEN: 'SUN',
-  morning: '上午',
-  morningTime: '09:00-12:00',
-  afternoon: '下午',
-  afternoonTime: '13:00-16:00',
-  evening: '晚上',
-  eveningTime: '18:30-21:30',
+  scheduleWeek: '門診時間/星期',
+  scheduleMonCH: '星期一',
+  scheduleMonEN: 'MON',
+  scheduleTueCH: '星期二',
+  scheduleTueEN: 'TUE',
+  scheduleWedCH: '星期三',
+  scheduleWedEN: 'WED',
+  scheduleThuCH: '星期四',
+  scheduleThuEN: 'THU',
+  scheduleFriCH: '星期五',
+  scheduleFriEN: 'FRI',
+  scheduleSatCH: '星期六',
+  scheduleSatEN: 'SAT',
+  scheduleSunCH: '星期日',
+  scheduleSunEN: 'SUN',
+  scheduleMorning: '上午',
+  scheduleMorningTime: '09:00-12:00',
+  scheduleAfternoon: '下午',
+  scheduleAfternoonTime: '13:00-16:00',
+  scheduleEvening: '晚上',
+  scheduleEveningTime: '18:30-21:30',
 })
 const {
-  week,
-  monCH,
-  monEN,
-  tueCH,
-  tueEN,
-  wedCH,
-  wedEN,
-  thuCH,
-  thuEN,
-  friCH,
-  friEN,
-  satCH,
-  satEN,
-  sunCH,
-  sunEN,
-  morning,
-  morningTime,
-  afternoon,
-  afternoonTime,
-  evening,
-  eveningTime,
+  scheduleWeek,
+  scheduleMonCH,
+  scheduleMonEN,
+  scheduleTueCH,
+  scheduleTueEN,
+  scheduleWedCH,
+  scheduleWedEN,
+  scheduleThuCH,
+  scheduleThuEN,
+  scheduleFriCH,
+  scheduleFriEN,
+  scheduleSatCH,
+  scheduleSatEN,
+  scheduleSunCH,
+  scheduleSunEN,
+  scheduleMorning,
+  scheduleMorningTime,
+  scheduleAfternoon,
+  scheduleAfternoonTime,
+  scheduleEvening,
+  scheduleEveningTime,
 } = toRefs(schedule)
+
 const ophthalmology = reactive({
-  title: '復健科門診',
-  note1:
+  ophthalmologyTitle: '復健科門診',
+  ophthalmologyNote1:
     '物理治療：冰凍肩(五十肩)、下背痛(腰痛)、坐骨神經痛、膝關節傷害、臏骨軟化症及其他膝蓋疼痛、腳踝扭傷等肌肉骨骼以及神經控制系統各種的不悅感或異常感。',
-  note2:
+  ophthalmologyNote2:
     '職能治療：成人治療範圍：腦傷、中風、脊椎損傷、骨折、燒燙傷等... / 兒童治療範圍：自閉症、過動症、腦性麻痺、發展遲緩、感覺統合失調、學習障礙等...。',
 })
+const { ophthalmologyTitle, ophthalmologyNote1, ophthalmologyNote2 } = toRefs(ophthalmology)
+
 const doctor = reactive({
   wang: '王司宏',
   xu: '徐郁芳',
@@ -60,6 +63,7 @@ const doctor = reactive({
 })
 const { wang, xu, huang } = toRefs(doctor)
 const rehabilitation = reactive({
+  name: '復健治療時間',
   morning: '上午',
   morningTime: '08:00-12:00',
   afternoon: '下午',
@@ -69,6 +73,7 @@ const rehabilitation = reactive({
   notice: '星期六至18:00 星期日至12:00',
 })
 const occupation = reactive({
+  name: '職能治療時間',
   child: '兒童',
   childDay: '星期一至五',
   childTime: '13:00 - 21:00',
@@ -80,13 +85,13 @@ const occupation = reactive({
 <template>
   <div class="scheduleOphthalmology">
     <div class="scheduleOphthalmology-title">
-      <p>{{ ophthalmology.title }}</p>
+      <p>{{ ophthalmologyTitle }}</p>
       <div>
         <p>
-          {{ ophthalmology.note1 }}
+          {{ ophthalmologyNote1 }}
         </p>
         <p>
-          {{ ophthalmology.note2 }}
+          {{ ophthalmologyNote2 }}
         </p>
       </div>
     </div>
@@ -95,43 +100,43 @@ const occupation = reactive({
         <thead>
           <tr>
             <th>
-              <p>{{ week }}</p>
+              <p>{{ scheduleWeek }}</p>
             </th>
             <th>
-              <p>{{ monCH }}</p>
-              <p>{{ monEN }}</p>
+              <p>{{ scheduleMonCH }}</p>
+              <p>{{ scheduleMonEN }}</p>
             </th>
             <th>
-              <p>{{ tueCH }}</p>
-              <p>{{ tueEN }}</p>
+              <p>{{ scheduleTueCH }}</p>
+              <p>{{ scheduleTueEN }}</p>
             </th>
             <th>
-              <p>{{ wedCH }}</p>
-              <p>{{ wedEN }}</p>
+              <p>{{ scheduleWedCH }}</p>
+              <p>{{ scheduleWedEN }}</p>
             </th>
             <th>
-              <p>{{ thuCH }}</p>
-              <p>{{ thuEN }}</p>
+              <p>{{ scheduleThuCH }}</p>
+              <p>{{ scheduleThuEN }}</p>
             </th>
             <th>
-              <p>{{ friCH }}</p>
-              <p>{{ friEN }}</p>
+              <p>{{ scheduleFriCH }}</p>
+              <p>{{ scheduleFriEN }}</p>
             </th>
             <th>
-              <p>{{ satCH }}</p>
-              <p>{{ satEN }}</p>
+              <p>{{ scheduleSatCH }}</p>
+              <p>{{ scheduleSatEN }}</p>
             </th>
             <th>
-              <p>{{ sunCH }}</p>
-              <p>{{ sunEN }}</p>
+              <p>{{ scheduleSunCH }}</p>
+              <p>{{ scheduleSunEN }}</p>
             </th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>
-              <p>{{ morning }}</p>
-              <p>{{ morningTime }}</p>
+              <p>{{ scheduleMorning }}</p>
+              <p>{{ scheduleMorningTime }}</p>
             </td>
             <td>
               <a href="/">
@@ -171,8 +176,8 @@ const occupation = reactive({
           </tr>
           <tr>
             <td>
-              <p>{{ afternoon }}</p>
-              <p>{{ afternoonTime }}</p>
+              <p>{{ scheduleAfternoon }}</p>
+              <p>{{ scheduleAfternoonTime }}</p>
             </td>
             <td>
               <a href="/">
@@ -212,8 +217,8 @@ const occupation = reactive({
           </tr>
           <tr>
             <td>
-              <p>{{ evening }}</p>
-              <p>{{ eveningTime }}</p>
+              <p>{{ scheduleEvening }}</p>
+              <p>{{ scheduleEveningTime }}</p>
             </td>
             <td>
               <a href="/">
@@ -255,7 +260,12 @@ const occupation = reactive({
       </table>
     </div>
     <div>
-      <p>123</p>
+      <div>
+        <p>{{ rehabilitation.name }}</p>
+      </div>
+      <div>
+        <p>{{ occupation.name }}</p>
+      </div>
     </div>
   </div>
 </template>
