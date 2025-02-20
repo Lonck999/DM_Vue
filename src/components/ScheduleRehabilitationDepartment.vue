096a9f7f6b6ceccec640c723111f3d7b4249e815
地瓜球工程師 <joe22053814@gmail.com>
2025-02-20 18:05:12 +0800

    Update ScheduleRehabilitationDepartment.vue

--- src/components/ScheduleRehabilitationDepartment.vue
+++ src/components/ScheduleRehabilitationDepartment.vue
@@ -1,52 +1,10 @@
 <script setup>
+import { useTableStore } from '@/stores/table'
+import { storeToRefs } from 'pinia'
 import { reactive, toRefs } from 'vue'
-const schedule = reactive({
-  scheduleWeek: '門診時間/星期',
-  scheduleMonCH: '星期一',
-  scheduleMonEN: 'MON',
-  scheduleTueCH: '星期二',
-  scheduleTueEN: 'TUE',
-  scheduleWedCH: '星期三',
-  scheduleWedEN: 'WED',
-  scheduleThuCH: '星期四',
-  scheduleThuEN: 'THU',
-  scheduleFriCH: '星期五',
-  scheduleFriEN: 'FRI',
-  scheduleSatCH: '星期六',
-  scheduleSatEN: 'SAT',
-  scheduleSunCH: '星期日',
-  scheduleSunEN: 'SUN',
-  scheduleMorning: '上午',
-  scheduleMorningTime: '09:00-12:00',
-  scheduleAfternoon: '下午',
-  scheduleAfternoonTime: '13:00-16:00',
-  scheduleEvening: '晚上',
-  scheduleEveningTime: '18:30-21:30',
-})
-const {
-  scheduleWeek,
-  scheduleMonCH,
-  scheduleMonEN,
-  scheduleTueCH,
-  scheduleTueEN,
-  scheduleWedCH,
-  scheduleWedEN,
-  scheduleThuCH,
-  scheduleThuEN,
-  scheduleFriCH,
-  scheduleFriEN,
-  scheduleSatCH,
-  scheduleSatEN,
-  scheduleSunCH,
-  scheduleSunEN,
-  scheduleMorning,
-  scheduleMorningTime,
-  scheduleAfternoon,
-  scheduleAfternoonTime,
-  scheduleEvening,
-  scheduleEveningTime,
-} = toRefs(schedule)
 
+const tableStore = useTableStore()
+const { scheduleWeek } = storeToRefs(tableStore.schedule)
 const ophthalmology = reactive({
   ophthalmologyTitle: '復健科門診',
   ophthalmologyNote1:
