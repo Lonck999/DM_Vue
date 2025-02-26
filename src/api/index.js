a8c161474f49d8b80a23905273884251cba5fdf8
地瓜球工程師 <joe22053814@gmail.com>
2025-02-26 16:39:04 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,31 +1,28 @@
 import axios from 'axios'
 
-const url = 'https://www.dltech.com.tw/DLWabAPI-Official/api/Authoutication/AccessToken'
+export default {
+  async getAccessToken() {
+    const url = 'https://www.dltech.com.tw/DLWabAPI-Official/api/Authoutication/AccessToken'
+    const params = {
+      API_ID: 'DM',
+      API_Secret: '08727379',
+    }
+    const headers = {
+      Accept: 'application/json',
+    }
 
-const getAccessToken = async () => {
-  const params = {
-    API_ID: 'DM',
-    API_Secret: '08727379',
-  }
-  const headers = {
-    Accept: 'application/json',
-  }
-
-  try {
-    const response = await axios.get(url, { headers, params })
-    if (response.data.Success) {
-      // 假設 token 位於回應的第一筆資料的 Token 欄位
-      return response.data.Data[0].Token
-    } else {
-      console.error('未能成功取得 Token:', response.data.Cont)
+    try {
+      const response = await axios.get(url, { headers, params })
+      if (response.data.Success) {
+        const date = response.data.Data[0]
+        return date
+      } else {
+        console.error('未能成功取得 Token:', response.data.Cont)
+        return null
+      }
+    } catch (error) {
+      console.error('請求失敗:', error)
       return null
     }
-  } catch (error) {
-    console.error('請求失敗:', error)
-    return null
-  }
-}
-
-export default {
-  getAccessToken,
+  },
 }
