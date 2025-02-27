f5e34b220075b6c31c6153238f15f37a6286570c
地瓜球工程師 <joe22053814@gmail.com>
2025-02-27 14:06:22 +0800

    Update Team.vue

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -1,6 +1,16 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+const title = ref('醫療團隊')
+</script>
 <template>
-  <div></div>
+  <section class="team">
+    <h1>{{ title }}</h1>
+  </section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.team {
+  width: 100%;
+  height: 100%;
+}
+</style>
