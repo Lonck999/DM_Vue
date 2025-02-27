a19c743f099c912652ef07c27ec7bffab2770508
地瓜球工程師 <joe22053814@gmail.com>
2025-02-27 15:15:25 +0800

    Update Team.vue

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -1,10 +1,82 @@
 <script setup>
-import { ref } from 'vue'
+import wangSiHong from '@/assets/img/wang-si-hong.jpg'
+import huangRuiBing from '@/assets/img/huang-rui-bing.jpg'
+import xuYuFang from '@/assets/img/xu-yu-fang.jpg'
+import caiZhengDa from '@/assets/img/cai-zheng-da.jpg'
+import lincCangJin from '@/assets/img/linc-cang-jin.jpg'
+import shiXingShan from '@/assets/img/shi-xing-shan.jpg'
+import qiuXuXiang from '@/assets/img/qiu-xu-xiang.jpg'
+import zhangShuJing from '@/assets/img/zhang-shu-jing.jpg'
+import liMingZhi from '@/assets/img/li-ming-zhi.jpg'
+import linJiaYong from '@/assets/img/lin-jia-yong.jpg'
+import { ref, reactive } from 'vue'
 const title = ref('醫療團隊')
+const team = reactive([
+  {
+    name: '王司宏',
+    jobTitle: '院長',
+    img: wangSiHong,
+  },
+  {
+    name: '黃瑞冰',
+    jobTitle: '醫師',
+    img: huangRuiBing,
+  },
+  {
+    name: '徐郁芳',
+    jobTitle: '醫師',
+    img: xuYuFang,
+  },
+  {
+    name: '蔡正達',
+    jobTitle: '醫師',
+    img: caiZhengDa,
+  },
+  {
+    name: '林昌錦',
+    jobTitle: '醫師',
+    img: lincCangJin,
+  },
+  {
+    name: '史幸仙',
+    jobTitle: '醫師',
+    img: shiXingShan,
+  },
+  {
+    name: '邱旭祥',
+    jobTitle: '醫師',
+    img: qiuXuXiang,
+  },
+  {
+    name: '張淑境',
+    jobTitle: '醫師',
+    img: zhangShuJing,
+  },
+  {
+    name: '李明智',
+    jobTitle: '醫師',
+    img: liMingZhi,
+  },
+  {
+    name: '林嘉勇',
+    jobTitle: '醫師',
+    img: linJiaYong,
+  },
+])
 </script>
 <template>
   <section class="team">
     <h1>{{ title }}</h1>
+    <div class="team-list">
+      <div v-for="item in team" :key="item.name" class="team-item">
+        <div class="team-item-img">
+          <img :src="item.img" alt="team-img" />
+        </div>
+        <p>
+          {{ item.name }} <span>{{ item.jobTitle }}</span>
+        </p>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -20,6 +92,29 @@ const title = ref('醫療團隊')
     color: $green-1;
     text-align: center;
     padding-top: 8rem;
+  }
+  .team-list {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+
+    .team-item {
+      width: 25%;
+      text-align: center;
+
+      .team-item-img {
+        width: 35rem;
+        height: 35rem;
+        border-radius: 50%;
+        overflow: hidden;
+        margin: 0 auto;
+
+        img {
+          width: 100%;
+          object-fit: contain;
+        }
+      }
+    }
   }
 }
 </style>
