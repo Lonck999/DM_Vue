2f00ded6ab54c0be3dcf713893ada44c044838f3
地瓜球工程師 <joe22053814@gmail.com>
2025-02-27 13:56:06 +0800

    Update nowNumber.js

--- src/api/nowNumber.js
+++ src/api/nowNumber.js
@@ -3,13 +3,13 @@ import apiData from './index'
 
 export default {
   async getNowNumber() {
-    const date = await apiData.getAccessToken()
-    const token = date.Token
+    const data = await apiData.getAccessToken()
+    const dataToken = data.Token
     const url = 'https://www.dltech.com.tw/DLWabAPI-Official/api/Official/NumberInfo'
     const os = 'Web'
     const ver = '1'
     const headers = {
-      token: token,
+      token: dataToken,
       os: os,
       ver: ver,
     }
