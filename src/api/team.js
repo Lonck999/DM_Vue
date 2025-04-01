17eb66111ebd3a9ebf263472809f2b14648726c3
地瓜球工程師 <joe22053814@gmail.com>
2025-04-01 17:46:51 +0800

    Update team.js

--- src/api/team.js
+++ src/api/team.js
@@ -4,20 +4,21 @@ import apiData from './index'
 export default async function getTeamData(drName) {
   const data = await apiData.getAccessToken()
   const dataToken = data.Token
+  const url = 'https://www.dltech.com.tw/DLWabAPI-Official/api/Official/DrDetail'
+  const os = 'Web'
+  const ver = '1'
+  const headers = {
+    token: dataToken,
+    os: os,
+    ver: ver,
+  }
   try {
-    const response = await axios.get(
-      'https://www.dltech.com.tw/DLWabAPI-Official/api/Official/DrDetail',
-      {
-        headers: {
-          token: dataToken,
-          os: 'web',
-          ver: '1',
-        },
-        params: {
-          DrName: drName,
-        },
+    const response = await axios.get(url, {
+      headers: headers,
+      params: {
+        DrName: drName,
       },
-    )
+    })
     return response.data
   } catch (error) {
     console.error('API 請求失敗:', error)
